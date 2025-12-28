6b253ed2c39b2c09175c04a527e9762b9b9a507b
ningengakushu-dot <ningengakushu@gmail.com>
2025-12-28 19:27:57 +0900

    リファクタリング1・　グーチョキパーまとめた

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// 23:43 mojos 3回連続で勝ったら修了するじゃんけんアプリ、ただし、あいこはリセットされずに継続
-// 23:45 mojos 勝った数と負けた数を最後に出力
-// 23:53 mojos aikoの場合は、あいこ　価値なら価値　という文字を出力
-/*
--- 第１弾 -- 勝ち負けなし、結果のみ表示
-プッシュ（グーチョキパーのいずれか）をクリックする
-ランダムで文字列のぐー・チョキ・パーが決定し、変数ナニカに代入される
-ドキュメントoutputを書き換える
-
--- 第２弾 -- 3回連続で勝ったら終了
-勝つとArrに+1、負けるとArrが0に戻る、3連続勝利（katiArr=3）になるまで繰り返し
-
--- 第３弾 -- リセットボタン（YES or NO）を追加
-*/
-
 let output = document.getElementById('output')
 let coment = document.getElementById('coment')
 let jan = document.getElementById('jan')
@@ -21,87 +6,48 @@ let make = 0
 let wake = 0
 let katiArr = 0
 
-function goo() {
+
+function playGame(userHand) {
+    // 画面表示をリセット
     output.innerHTML = ""
     coment.innerHTML = ""
-    let janNumber = Math.floor(Math.random() * 3);
-    console.log(`連続勝利：${katiArr}`)
-    
-    if(janNumber === 0){
+
+    // 相手の手 ランダム（0:グー, 1:チョキ, 2:パー）
+    let enemyHand = Math.floor(Math.random() * 3);
+
+    // 画面に自分の手を表示
+    if (userHand === 0) {
         output.innerHTML = "グー"
+    } else if (userHand === 1) {
+        output.innerHTML = "チョキ"
+    } else if (userHand === 2) {
+        output.innerHTML = "パー"
+    }
+    console.log(`自分の手: ${userHand}, 相手の手: ${enemyHand}`);
+
+    // あいこ
+    if (userHand === enemyHand) {
         wake += 1
         coment.innerHTML = `あいこ！もう一回！<br>勝：${kati}　負：${make}　引き分け：${wake}`
     }
-    if(janNumber === 1){
-        output.innerHTML = "チョキ"
+    // 勝ち
+    else if (userHand === 0 && enemyHand === 1 ||
+        userHand === 1 && enemyHand === 2 ||
+        userHand === 2 && enemyHand === 0) {
         kati += 1
         katiArr += 1
         coment.innerHTML = `勝ち！<br>勝：${kati}　負：${make}　引き分け：${wake}`
     }
-    if(janNumber === 2){
-        output.innerHTML = "パー"
+    // まけ
+    else if (userHand === 0 && enemyHand === 2 ||
+        userHand === 1 && enemyHand === 0 ||
+        userHand === 2 && enemyHand === 1) {
         make += 1
         katiArr = 0
-        coment.innerHTML = `敗北...<br>勝：${kati}　負：${make}　引き分け：${wake}`
+        coment.innerHTML = `敗北！<br>勝：${kati}　負：${make}　引き分け：${wake}`
     }
 
-    if(katiArr === 3){
-        jan.innerHTML = `★ YOU WIN !! ★ おつかれさま！`
-    }
-}
-
-function choki() {
-    output.innerHTML = ""
-    coment.innerHTML = ""
-    let janNumber = Math.floor(Math.random() * 3);
-    console.log(janNumber)
-    if(janNumber === 0){
-        output.innerHTML = "グー"
-        make += 1
-        katiArr = 0
-        coment.innerHTML = `敗北...<br>勝：${kati}　負：${make}　引き分け：${wake}`
-    }
-    if(janNumber === 1){
-        output.innerHTML = "チョキ"
-        wake += 1
-        coment.innerHTML = `あいこ！もう一回！<br>勝：${kati}　負：${make}　引き分け：${wake}`
-    }
-    if(janNumber === 2){
-        output.innerHTML = "パー"
-        kati += 1
-        katiArr += 1
-        coment.innerHTML = `勝ち！<br>勝：${kati}　負：${make}　引き分け：${wake}`
-    }
-
-    if(katiArr === 3){
-        jan.innerHTML = `★ YOU WIN !! ★ おつかれさま！`
-    }
-}
-
-function par() {
-    output.innerHTML = ""
-    coment.innerHTML = ""
-    let janNumber = Math.floor(Math.random() * 3);
-    console.log(janNumber)
-    if(janNumber === 0){
-        output.innerHTML = "グー"
-        kati += 1
-        katiArr += 1
-        coment.innerHTML = `勝ち！<br>勝：${kati}　負：${make}　引き分け：${wake}`
-    }
-    if(janNumber === 1){
-        output.innerHTML = "チョキ"
-        make += 1
-        katiArr = 0
-        coment.innerHTML = `敗北...<br>勝：${kati}　負：${make}　引き分け：${wake}`
-    }
-    if(janNumber === 2){
-        output.innerHTML = "パー"
-        wake += 1
-        coment.innerHTML = `あいこ！もう一回！<br>勝：${kati}　負：${make}　引き分け：${wake}`
-    }
-
-    if(katiArr === 3){
+    if (katiArr === 3) {
         jan.innerHTML = `★ YOU WIN !! ★ おつかれさま！`
     }
 }
@@ -113,8 +59,8 @@ function reset() {
     katiArr = 0
     output.innerHTML = ""
     coment.innerHTML = `<div id="coment">いざ、勝負！</div>`
-    jan.innerHTML = 
-    `<div id="goo" class="hand" onclick="goo()">グー</div>
-    <div id="choki" class="hand" onclick="choki()">チョキ</div>
-    <div id="par" class="hand" onclick="par()">パー</div>`
+    jan.innerHTML =
+        `<div id="goo" class="hand" onclick="playGame(0)">グー</div>
+    <div id="choki" class="hand" onclick="playGame(1)">チョキ</div>
+    <div id="par" class="hand" onclick="playGame(2)">パー</div>`
 }
